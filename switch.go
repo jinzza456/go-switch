590b74a8f5bb0ddbccf667f6c0b0df8f3b9f9be2
jinzza456 <jinzza456@gmail.com>
2021-10-12 09:53:13 +0900

    switch ture

--- switch.go
+++ switch.go
@@ -5,12 +5,23 @@ import "fmt"
 func main() {
 	x := 33
 
-	switch x {
-	case 31:
-		fmt.Println("x = 31")
-	case 32:
-		fmt.Println("x = 32")
-	case 33:
-		fmt.Println("x = 33")
+	// switch x {
+	// case 31:
+	// 	fmt.Println("x = 31")
+	// case 32:
+	// 	fmt.Println("x = 32")
+	// case 33:
+	// 	fmt.Println("x = 33")
+	// }
+
+	switch {
+	case x > 40:
+		fmt.Println("x 는 40보다 크다")
+	case x < 20:
+		fmt.Println("x는 20보다 작다")
+	case x > 30:
+		fmt.Println("x는 30보다 크다")
+	case x == 33:
+		fmt.Println("x 는 33")
 	}
 }
